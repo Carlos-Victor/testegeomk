45f8f70a9b562c3af70ddf8e7316a1121a2cf443
VcTrHub <48018920+VcTrHub@users.noreply.github.com>
2019-12-01 16:17:21 -0300

    Adionado config database Postgres

--- source/startproject/settings.py
+++ source/startproject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'startproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['POSTGRESQL_DATABASE'],
+        'USER': os.environ['POSTGRESQL_USER'],
+        'PASSWORD': os.environ['POSTGRESQL_PASSWORD'],
+        'HOST': os.environ['POSTGRESQL_HOST'],
+        'PORT': os.environ['POSTGRESQL_PORT']
     }
 }
 
